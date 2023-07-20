2a00c08c93ab7435f7ed280dd914c1ca67b4e6b7
52rondo <52rondo@gmail.com>
2023-07-20 20:11:49 +0800

    initial project

---
+++ src/main/java/com/rondo/sample_terry/SampleTerryApplication.java
@@ -0,0 +1,13 @@
+package com.rondo.sample_terry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTerryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTerryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rondo/sample_terry/SampleTerryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rondo.sample_terry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTerryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
